f5e3b821602dc589e4a922afdc8ea54c89a5c284
Seong_gu <sunggu507@naver.com>
2024-08-21 11:54:38 +0900

    feat : restaurant validation 추가

--- src/main/java/com/kcc/reviewservice/entity/Restaurant.java
+++ src/main/java/com/kcc/reviewservice/entity/Restaurant.java
@@ -1,5 +1,8 @@
 package com.kcc.reviewservice.entity;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,10 +15,14 @@ import java.io.Serializable;
 @AllArgsConstructor
 public class Restaurant implements Serializable {
 
+
     private int id;
 
+    @NotNull
+    @Size(min = 1, max = 20)
     private String name;
 
+    @Pattern(regexp = "(([가-힣]|\\\\d)+(시 ))+(([가-힣]|\\\\d)+(구))")
     private String address;
 
     private String created_at;
